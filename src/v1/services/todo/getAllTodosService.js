d38a08edd28f502da4866c4155a14601d6bfda8a
Duc Bao <ducbao_tran@datahouse.com>
2025-04-16 14:00:07 +0700

    feat: remove redundant code

--- src/v1/services/todo/getAllTodosService.js
+++
@@ -1,7 +0,0 @@
-const prisma = require('../../../utils/prisma');
-
-const getAllTodosService = async (settings = {}) => {
-  return await prisma.todo.findMany(settings);
-};
-
-module.exports = getAllTodosService;
