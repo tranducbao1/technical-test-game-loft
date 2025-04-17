9aa4fd79929966f6a217bc3f9e6d125fe64497ca
Duc Bao <ducbao_tran@datahouse.com>
2025-04-17 22:23:22 +0700

    feat: prisma service

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -3,11 +3,33 @@ const { PrismaClient } = require('@prisma/client');
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    __internal: {
+      useUds: false,
+      engine: {
+        enableExperimentalFeatures: [],
+        env: {
+          PRISMA_DISABLE_PREPARED_STATEMENTS: 'true',
+        },
+      },
+    },
+  });
 } else {
-  // Sử dụng biến toàn cục để tránh tạo nhiều instance trong dev hoặc serverless
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({
+      __internal: {
+        engine: {
+          env: {
+            PRISMA_DISABLE_PREPARED_STATEMENTS: 'true',
+          },
+        },
+      },
+    });
   }
   prisma = global.prisma;
 }
