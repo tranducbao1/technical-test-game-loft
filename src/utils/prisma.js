d2766d914e8c394d2a5380fd87622ee0a64553a4
Duc Bao <ducbao_tran@datahouse.com>
2025-04-17 22:30:30 +0700

    feat: prisma service

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -3,33 +3,10 @@ const { PrismaClient } = require('@prisma/client');
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-    __internal: {
-      useUds: false,
-      engine: {
-        enableExperimentalFeatures: [],
-        env: {
-          PRISMA_DISABLE_PREPARED_STATEMENTS: 'true',
-        },
-      },
-    },
-  });
+  prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      __internal: {
-        engine: {
-          env: {
-            PRISMA_DISABLE_PREPARED_STATEMENTS: 'true',
-          },
-        },
-      },
-    });
+    global.prisma = new PrismaClient();
   }
   prisma = global.prisma;
 }
